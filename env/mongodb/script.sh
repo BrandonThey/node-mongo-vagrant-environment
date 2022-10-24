3d5e1d61e6fba88f801cf060247fe05be957d257
Brandon They <theyb5220@gmail.com>
2022-10-24 14:17:31 -0500

    Added gitignore to ignore vagrant

--- env/mongodb/script.sh
+++ env/mongodb/script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo -------------- Import the public key for APT ---------------
-sudo wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+sudo wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 
 echo ---------------- Create list file for MongoDV --------------
-sudo echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
 echo ------------------------ Update packages -------------------
 sudo apt-get update
